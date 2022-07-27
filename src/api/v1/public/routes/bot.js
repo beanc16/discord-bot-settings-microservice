7b3a83236bb938fe5faf1a85ca005c971970fbcf
beanc16 <Bean.Christopher@outlook.com>
2022-07-26 22:20:54 -0400

    Make create bot endpoint

--- src/api/v1/public/routes/bot.js
+++ src/api/v1/public/routes/bot.js
@@ -7,8 +7,39 @@ const express = require("express");
 const app = express();
 
 
+// Access req.body in post requests
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());                         // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/xwww-form-urlencoded
+
+
+// Telemetry
+const { logger } = require("@beanc16/logger");
+
+
+// Microservices
+const { AppMicroservice } = require("@beanc16/microservices-abstraction");
+
+
+// Models
+const { Bot } = require("../../../../js/models");
+
+
+// Controllers
+const { BotController } = require("../../../../js/controllers");
+
+
+// Validation
+const {
+    validateCreateBotPayload,
+} = require("../validation");
+
+
 // Response
-const { Success } = require("dotnet-responses");
+const {
+    Success,
+    InternalServerError,
+} = require("dotnet-responses");
 
 
 
@@ -46,12 +77,50 @@ app.get("/:appId/:serverId", function(req, res)
 
 app.post("/create", function(req, res)
 {
-    // TODO: Create discord bot as app in app microservice.
+    validateCreateBotPayload(req.body)
+    .then(function (_)
+    {
+        AppMicroservice.v1.create(req.body.app)
+        .then(function (result)
+        {
+            const app = result.data.data[0];
+            const bot = new Bot({
+                appId: app._id,
+                servers: req.body.servers,
+                data: req.body.data,
+            });
+
+            BotController.insertOne(bot)
+            .then((result) => _sendBotCreateSuccess(res, result))
+            .catch((err) => _sendBotCreateError(res, err));
+        })
+        .catch((err) => _sendAppMicroserviceError(req, res, err));
+    })
+    .catch((err) => _sendPayloadValidationError(res, err));
+});
+
+function _sendBotCreateSuccess(res, result)
+{
     Success.json({
         res,
-        message: "Pong",
+        message: "Successfully created bot",
+        data: result,
     });
-});
+}
+
+function _sendBotCreateError(res, err)
+{
+    const errMsg = "Failed to create bot";
+    logger.error(errMsg, err);
+
+    InternalServerError.json({
+        res,
+        message: errMsg,
+        error: err,
+    });
+}
+
+
 
 app.post("/:appId", function(req, res)
 {
@@ -61,6 +130,70 @@ app.post("/:appId", function(req, res)
         message: "Pong",
     });
 });
+
+
+
+
+
+/***********
+ * HELPERS *
+ ***********/
+
+function _sendQueryValidationError(res, err)
+{
+    ValidationError.json({
+        res,
+        message: "Query Validation Error",
+        error: err,
+    });
+}
+
+function _sendPayloadValidationError(res, err)
+{
+    ValidationError.json({
+        res,
+        error: err,
+    });
+}
+
+function _sendAppMicroserviceError(req, res, err)
+{
+    const statusCode = (err && err.response && err.response.status)
+        ? err.response.status
+        : 500;
+
+    if (statusCode === 422)
+    {
+        ValidationError.json({
+            res,
+            data: {
+                ...req.query,
+            },
+            error: err.response.data.error,
+        });
+    }
+
+    else
+    {
+        const errMsg = "An unknown error occurred while creating an app";
+        logger.error(errMsg, err.response.data.error || err, req.query, err.response.data);
+
+        InternalServerError.json({
+            res,
+            message: errMsg,
+            data: req.query,
+            error: err.response.data.error || err,
+        });
+    }
+}
+
+function _getAppDataFromQuery(req)
+{
+    return {
+        id: (req.query.appId) || process.env.FILE_STORAGE_MICROSERVICE_APP_ID || undefined,
+        searchName: req.query.appName || undefined,
+    };
+}
 
 
 
