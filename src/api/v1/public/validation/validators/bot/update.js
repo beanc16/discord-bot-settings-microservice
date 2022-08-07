7085fa67eed6b285b6076a6639ec6ec66ac680bf
beanc16 <bean.christopher@outlook.com>
2022-08-07 15:16:08 -0400

    Reorder functions

--- src/api/v1/public/validation/validators/bot/update.js
+++ src/api/v1/public/validation/validators/bot/update.js
@@ -3,22 +3,6 @@ const { validateJoiSchema } = require("@beanc16/joi-helpers");
 
 
 
-function validateUpsertBotPrefixPayload(payload)
-{
-    return new Promise(function (resolve, reject)
-    {
-        validateJoiSchema(upsertBotPrefixSchema, payload)
-            .then(function (value)
-            {
-                resolve(value);
-            })
-            .catch(function (error)
-            {
-                reject(error);
-            });
-    });
-}
-
 function validateUpsertBotDataPayload(payload)
 {
     return new Promise(function (resolve, reject)
@@ -35,9 +19,25 @@ function validateUpsertBotDataPayload(payload)
     });
 }
 
+function validateUpsertBotPrefixPayload(payload)
+{
+    return new Promise(function (resolve, reject)
+    {
+        validateJoiSchema(upsertBotPrefixSchema, payload)
+            .then(function (value)
+            {
+                resolve(value);
+            })
+            .catch(function (error)
+            {
+                reject(error);
+            });
+    });
+}
+
 
 
 module.exports = {
-    validateUpsertBotPrefixPayload,
     validateUpsertBotDataPayload,
+    validateUpsertBotPrefixPayload,
 };
